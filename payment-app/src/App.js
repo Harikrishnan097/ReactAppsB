b8a6a74db092452334c5af2b1b0d0554db16aa92
Harikrishnan <31622792+Harikrishnan097@users.noreply.github.com>
2021-09-01 10:17:14 +0530

    upload with node module empty
    while import instal npm install on project folder

---
+++ payment-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PaymentForm from './component/paymentform/paymentform';
+
+function App() {
+  return (
+    
+
+<PaymentForm></PaymentForm>
+
+  );
+}
+
+export default App;
